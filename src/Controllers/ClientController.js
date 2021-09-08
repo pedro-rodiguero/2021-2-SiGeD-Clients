e9bceb03e91f559558597dd72ddfb0de0d26bb7f
Gustavo Marques <gustavomarques.gml@gmail.com>
2021-09-08 14:38:41 -0300

    [8] addition of send-email request

--- src/Controllers/ClientController.js
+++ src/Controllers/ClientController.js
@@ -1,12 +1,13 @@
-const moment = require('moment-timezone');
-const Client = require('../Models/ClientSchema');
-const validation = require('../Utils/validate');
-const verifyChanges = require('../Utils/verifyChanges');
-const { getUser } = require('../Services/Axios/userService');
+const moment = require("moment-timezone");
+const Client = require("../Models/ClientSchema");
+const validation = require("../Utils/validate");
+const mailer = require("../Utils/mailer");
+const verifyChanges = require("../Utils/verifyChanges");
+const { getUser } = require("../Services/Axios/userService");
 
 const accessList = async (req, res) => {
   const { active } = req.query;
-  if (active === 'false') {
+  if (active === "false") {
     const clients = await Client.find({ active });
     return res.json(clients);
   }
@@ -20,32 +21,52 @@ const access = async (req, res) => {
   const { id } = req.params;
 
   try {
-    const client = await Client.findOne({ _id: id }).populate('lotacao');
+    const client = await Client.findOne({ _id: id }).populate("lotacao");
     return res.json(client);
   } catch (error) {
-    return res.status(400).json({ message: 'Client not found' });
+    return res.status(400).json({ message: "Client not found" });
   }
 };
 
 const create = async (req, res) => {
   const {
-    name, cpf, email, phone, secondaryPhone, address,
-    office, active, location, userID, features, image,idLotacao
+    name,
+    cpf,
+    email,
+    phone,
+    secondaryPhone,
+    address,
+    office,
+    active,
+    location,
+    userID,
+    features,
+    image,
+    idLotacao,
   } = req.body;
-  const errorMessage = validation.validate(name, cpf, email, phone, secondaryPhone, office);
+  const errorMessage = validation.validate(
+    name,
+    cpf,
+    email,
+    phone,
+    secondaryPhone,
+    office
+  );
 
   if (errorMessage.length) {
     return res.status(400).json({ message: errorMessage });
   }
 
   try {
-    const token = req.headers['x-access-token'];
+    const token = req.headers["x-access-token"];
     const user = await getUser(userID, token);
 
     if (user.error) {
       return res.status(400).json({ message: user.error });
     }
-    const date = moment.utc(moment.tz('America/Sao_Paulo').format('YYYY-MM-DDTHH:mm:ss')).toDate();
+    const date = moment
+      .utc(moment.tz("America/Sao_Paulo").format("YYYY-MM-DDTHH:mm:ss"))
+      .toDate();
     const client = await Client.create({
       name,
       cpf,
@@ -60,14 +81,14 @@ const create = async (req, res) => {
       history: {
         userID,
         date,
-        label: 'created',
+        label: "created",
       },
       image,
-      lotacao:idLotacao,
+      lotacao: idLotacao,
       createdAt: date,
       updatedAt: date,
     });
-    return res.json(client).populate('lotacao')
+    return res.json(client).populate("lotacao");
   } catch (error) {
     return res.status(400).json({ message: error.keyValue });
   }
@@ -76,18 +97,35 @@ const create = async (req, res) => {
 const update = async (req, res) => {
   const { id } = req.params;
   const {
-    name, cpf, email, phone, secondaryPhone, office,
-    address, location, userID, features, image,idLotacao
+    name,
+    cpf,
+    email,
+    phone,
+    secondaryPhone,
+    office,
+    address,
+    location,
+    userID,
+    features,
+    image,
+    idLotacao,
   } = req.body;
 
-  const errorMessage = validation.validate(name, cpf, email, phone, secondaryPhone, office);
+  const errorMessage = validation.validate(
+    name,
+    cpf,
+    email,
+    phone,
+    secondaryPhone,
+    office
+  );
 
   if (errorMessage.length) {
     return res.status(400).json({ message: errorMessage });
   }
 
   try {
-    const token = req.headers['x-access-token'];
+    const token = req.headers["x-access-token"];
 
     const user = await getUser(userID, token);
 
@@ -96,24 +134,29 @@ const update = async (req, res) => {
     }
 
     const clientHistory = await verifyChanges(req.body, id);
-    const clientBeforeUpdate = await Client.findOneAndUpdate({ _id: id }, {
-      name,
-      cpf,
-      email,
-      phone,
-      secondaryPhone,
-      office,
-      location,
-      features,
-      address,
-      history: clientHistory,
-      image,
-      lotacao:idLotacao,
-      updatedAt: moment.utc(moment.tz('America/Sao_Paulo').format('YYYY-MM-DDTHH:mm:ss')).toDate(),
-    },
-    { new: true });
-    
-    const clientAfterUpdate = await Client.findById(id).populate('lotacao');
+    const clientBeforeUpdate = await Client.findOneAndUpdate(
+      { _id: id },
+      {
+        name,
+        cpf,
+        email,
+        phone,
+        secondaryPhone,
+        office,
+        location,
+        features,
+        address,
+        history: clientHistory,
+        image,
+        lotacao: idLotacao,
+        updatedAt: moment
+          .utc(moment.tz("America/Sao_Paulo").format("YYYY-MM-DDTHH:mm:ss"))
+          .toDate(),
+      },
+      { new: true }
+    );
+
+    const clientAfterUpdate = await Client.findById(id).populate("lotacao");
     return res.json(clientAfterUpdate);
   } catch (error) {
     return res.status(400).json({ duplicated: error.keyValue });
@@ -129,21 +172,25 @@ const toggleStatus = async (req, res) => {
     let { active } = clientFound;
 
     if (!validation.validateActive(active)) {
-      return res.status(400).json({ message: 'invalid active value' });
+      return res.status(400).json({ message: "invalid active value" });
     }
 
     active = !clientFound.active;
 
-    const updateReturn = await Client.findOneAndUpdate({ _id: id }, { active },
-      { new: true }, (err, client) => {
+    const updateReturn = await Client.findOneAndUpdate(
+      { _id: id },
+      { active },
+      { new: true },
+      (err, client) => {
         if (err) {
           return res.status(400).json(err);
         }
         return res.json(client);
-      });
+      }
+    );
     return updateReturn;
   } catch (err) {
-    return res.status(400).json({ message: 'Client not found' });
+    return res.status(400).json({ message: "Client not found" });
   }
 };
 
@@ -151,34 +198,36 @@ const history = async (req, res) => {
   const { id } = req.params;
 
   try {
-    let error = '';
-    const token = req.headers['x-access-token'];
+    let error = "";
+    const token = req.headers["x-access-token"];
     const clientFound = await Client.findOne({ _id: id });
-    const clientHistory = await Promise.all(clientFound.history.map(async (elem) => {
-      const user = await getUser(elem.userID, token);
-      if (user.error) {
-        error = user.error;
-        return;
-      }
-      return {
-        label: elem.label,
-        before: elem.before,
-        after: elem.after,
-        date: elem.date,
-        user: {
-          _id: user._id,
-          name: user.name,
-          sector: user.sector,
-          role: user.role,
-        },
-      };
-    }));
+    const clientHistory = await Promise.all(
+      clientFound.history.map(async (elem) => {
+        const user = await getUser(elem.userID, token);
+        if (user.error) {
+          error = user.error;
+          return;
+        }
+        return {
+          label: elem.label,
+          before: elem.before,
+          after: elem.after,
+          date: elem.date,
+          user: {
+            _id: user._id,
+            name: user.name,
+            sector: user.sector,
+            role: user.role,
+          },
+        };
+      })
+    );
     if (error) {
       return res.status(400).json({ message: error });
     }
     return res.json(clientHistory);
   } catch {
-    return res.status(400).json({ message: 'Client not found' });
+    return res.status(400).json({ message: "Client not found" });
   }
 };
 
@@ -188,6 +237,38 @@ const newestFourClientsGet = async (req, res) => {
   return res.status(200).json(clients);
 };
 
+const sendEmailToClient = async (req, res) => {
+  const { id } = req.params;
+  const { transporter } = mailer;
+  const { subject, text } = req.body;
+  console.log(req, res);
+  try {
+    const client = await Client.findOne({ _id: id });
+    if (!client) {
+      return res.status(404).json({
+        error: "It was not possible to find an user with this email.",
+      });
+    }
+
+    transporter.sendMail({
+      from: process.env.email,
+      to: client.email,
+      subject: subject,
+      text: text,
+    });
+
+    return res.json({ message: "Email sent." });
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+};
 module.exports = {
-  accessList, access, create, update, toggleStatus, history, newestFourClientsGet,
+  accessList,
+  access,
+  create,
+  update,
+  toggleStatus,
+  history,
+  newestFourClientsGet,
+  sendEmailToClient,
 };
