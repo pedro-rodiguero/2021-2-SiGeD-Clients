5362a2d55a3bc5ccfb228fc428c5a8ab7b0a6594
DaviSilva25 <daviunb25@gmail.com>
2022-04-13 01:54:34 -0300

    Fixing client with demand deletion
    Co-authored-by: Karla Feliciano <karla.s.feliciano@gmail.com>

---
+++ src/Services/Axios/demandService.js
@@ -0,0 +1,36 @@
+const { APIDemands } = require('./baseService');
+
+const getDemands = async (token) => {
+  try {
+    const demands = await APIDemands.get('/demand', {
+      headers: { 'x-access-token': token },
+    }).then((response) => response.data);
+    return demands;
+  } catch (err) {
+    console.log(err);
+    return { error: 'Could not connect to api_demands' };
+  }
+};
+
+// const sendEmailToClient = async (
+//   clientId,
+//   subject,
+//   text,
+//   token,
+//   dateString = '',
+// ) => {
+//   const body = { subject, text, dateString };
+//   const response = await APIClients.post(
+//     `/clients/send-email/${clientId}`,
+//     body,
+//     {
+//       headers: { 'x-access-token': token },
+//     },
+//   );
+//   return response;
+// };
+
+module.exports = {
+  getDemands,
+  // sendEmailToClient,
+};
